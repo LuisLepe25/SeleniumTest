8b18d09935daa1e1c48472848b58441396e834da
Luis Lepe <luisdaniel.lepe@gmail.com>
2019-02-17 02:01:30 -0600

    Añadiendo logica para leer el archivo de datos

--- SeleniumTest/Form1.cs
+++ SeleniumTest/Form1.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -16,5 +18,65 @@ namespace SeleniumTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<Alumno> lstAlumnos = CargarListas();
+            if(lstAlumnos != null)
+            {
+
+            }
+        }
+
+        private List<Alumno> CargarListas()
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.Filter = "Text Files|*.txt";
+            openFileDialog1.Title = "Selecciona el archivo con la información";
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                Stream file = openFileDialog1.OpenFile();
+                List<Alumno> lstAlumnos = new List<Alumno>();
+                using (StreamReader sr = new StreamReader(file, Encoding.UTF8))
+                {
+                    try
+                    {
+                        while (!sr.EndOfStream)
+                        {
+                            try
+                            {
+                                String line = sr.ReadLine();
+                                String[] lineSplit = line.Split('\t');
+                                Regex rMatricula = new Regex("^[0-9]*$");
+                                if (rMatricula.IsMatch(lineSplit[0]))
+                                {
+                                    UInt32.TryParse(lineSplit[0], out uint matricula);
+                                    String nombre = lineSplit[1];
+                                    Alumno a = new Alumno(matricula, nombre);
+                                    System.Diagnostics.Debug.WriteLine("Se agrego alumno. Matricula =" + matricula + ", Nombre=" + nombre);
+                                    lstAlumnos.Add(a);
+                                }
+                                else
+                                {
+                                    System.Diagnostics.Debug.WriteLine("FALLO AGREGAR LINEA. Matricula =" + lineSplit[0]);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                System.Diagnostics.Debug.WriteLine(ex.Message);
+                            }
+                        }
+                        return lstAlumnos;
+                    }
+                    catch (Exception ex)
+                    {
+                        System.Diagnostics.Debug.WriteLine(ex.Message);
+                        MessageBox.Show("Error al leer archivo. Ex:" + ex.Message, "Error fatal", MessageBoxButtons.OK);
+                    }
+                }
+            }
+            return null;
+        }
     }
 }
+
---
+++ SeleniumTest/Models/Alumno.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeleniumTest
+{
+    class Alumno
+    {
+        private UInt32 Matricula { get; set; }
+        private String Nombre { get; set; }
+
+        public Alumno(UInt32 Matricula, String Nombre)
+        {
+            this.Matricula = Matricula;
+            this.Nombre = Nombre;
+        }
+    }
+}
